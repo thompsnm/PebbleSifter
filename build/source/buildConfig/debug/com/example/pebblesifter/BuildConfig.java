4aa821849b9362dd3588190b172debbd194aac58
Nathan Thompson <thompsnm@gmail.com>
2013-10-27 11:27:14 -0600

    Restructured project

--- build/source/buildConfig/debug/com/example/pebblesifter/BuildConfig.java
+++ build/source/buildConfig/debug/com/example/pebblesifter/BuildConfig.java
@@ -1,8 +1,7 @@
-/*___Generated_by_IDEA___*/
-
 /** Automatically generated file. DO NOT MODIFY */
 package com.example.pebblesifter;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
 }
