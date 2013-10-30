841b75c7a1c9ca907af356d0a478030020e0bcfb
Nathan Thompson <thompsnm@gmail.com>
2013-10-29 22:05:18 -0600

    Working commit, commented out broken code in MainActivity

--- src/main/java/com/pebblesifter/MainActivity.java
+++ src/main/java/com/pebblesifter/MainActivity.java
@@ -3,21 +3,24 @@ package com.pebblesifter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.example.pebblesifter.R;
+import com.pebblesifter.sifters.PebbleSifter;
+import com.pebblesifter.sifters.TeamTriviaAnswerSifter;
 
 public class MainActivity extends Activity {
 
-	PebbleSifter sifter = new TeamTriviaAnswerSifter();
+//	PebbleSifter sifter = new TeamTriviaAnswerSifter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		TextView name = (TextView) findViewById(R.id.sifter_name);
-		name.setText(sifter.getName());
-		TextView name = (TextView) findViewById(R.id.sifted_text);
-		name.setText(sifter.sift());
+//		TextView name = (TextView) findViewById(R.id.sifter_name);
+//		name.setText("Name");
+//		TextView siftedText = (TextView) findViewById(R.id.sifted_text);
+//		siftedText.setText("Text");
     }
 
 
