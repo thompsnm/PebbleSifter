95fbdc8e90574f1f4ebb174aeb3f03a49f96afbc
Nathan Thompson <thompsnm@gmail.com>
2013-10-30 20:06:34 -0600

    Modified AsyncTask

--- src/main/java/com/pebblesifter/sifters/TeamTriviaAnswerSifter.java
+++ src/main/java/com/pebblesifter/sifters/TeamTriviaAnswerSifter.java
@@ -2,7 +2,6 @@ package com.pebblesifter.sifters;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
