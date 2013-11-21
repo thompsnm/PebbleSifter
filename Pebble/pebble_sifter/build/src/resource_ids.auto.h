6af96bc43a15722ca02bae7b402951a1838778c6
Nathan Thompson <thompsnm@gmail.com>
2013-11-20 22:39:13 -0700

    Testing app sync

--- Pebble/pebble_sifter/build/src/resource_ids.auto.h
+++ Pebble/pebble_sifter/build/src/resource_ids.auto.h
@@ -15,7 +15,7 @@ typedef enum {
 } ResourceId;
 static const ResBankVersion VERSION = {
   .crc = 2400173576,
-  .timestamp = 1385012224,
+  .timestamp = 1385012337,
   .friendly_version = "VERSION"
 };
 
