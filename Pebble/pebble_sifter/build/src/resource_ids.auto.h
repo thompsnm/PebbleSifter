e918bad616664f5c06e0daf32867496fcdcb0cc4
Nathan Thompson <thompsnm@gmail.com>
2013-11-20 22:04:17 -0700

    Rebuilt Pebble app

--- Pebble/pebble_sifter/build/src/resource_ids.auto.h
+++ Pebble/pebble_sifter/build/src/resource_ids.auto.h
@@ -15,7 +15,7 @@ typedef enum {
 } ResourceId;
 static const ResBankVersion VERSION = {
   .crc = 2400173576,
-  .timestamp = 1385010043,
+  .timestamp = 1385010222,
   .friendly_version = "VERSION"
 };
 
