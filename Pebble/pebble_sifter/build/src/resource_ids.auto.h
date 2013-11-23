a2e7267642e43de26c52d38038e22bec6fdf3120
Nathan Thompson <thompsnm@gmail.com>
2013-11-23 11:54:47 -0700

    Fixed scroll layer size

--- Pebble/pebble_sifter/build/src/resource_ids.auto.h
+++ Pebble/pebble_sifter/build/src/resource_ids.auto.h
@@ -15,7 +15,7 @@ typedef enum {
 } ResourceId;
 static const ResBankVersion VERSION = {
   .crc = 2400173576,
-  .timestamp = 1385043375,
+  .timestamp = 1385232862,
   .friendly_version = "VERSION"
 };
 
